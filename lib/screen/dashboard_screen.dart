0a9e19208c878037608049dada9dbd6595fa23b9
Hugo Rayhan Firmansyah <hugo.hgrayhan08@gmail.com>
2023-05-06 11:20:29 +0700

    memperbaiki halaman dashboard

--- lib/screen/dashboard_screen.dart
+++ lib/screen/dashboard_screen.dart
@@ -10,100 +10,118 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffA5D7E8),
-      // appBar: AppBar(
-      //   title: const Text("sahabat muslim"),
-      // ),
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/images/latarbelakang.jpg"),
+            fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        // backgroundColor: const Color(0xffA5D7E8),
+        // appBar: AppBar(
+        //   title: const Text("sahabat muslim"),
+        // ),
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                  color: Color(0xff8294C4),
                 ),
-                color: Color(0xff19376D),
+                width: double.infinity,
+                height: 240,
               ),
-              width: double.infinity,
-              height: 240,
-            ),
-            const Positioned(
-              top: 30,
-              left: 25,
-              child: Text(
-                "Teman Doa",
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+              const Positioned(
+                top: 30,
+                left: 25,
+                child: Text(
+                  "Teman Doa",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Column(
-              children: [
-                const SizedBox(
-                  height: 90,
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  height: 220,
-                  margin: const EdgeInsets.symmetric(horizontal: 15),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20),
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 90,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    height: 220,
+                    margin: const EdgeInsets.symmetric(horizontal: 15),
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                      image: const DecorationImage(
+                        image: AssetImage("assets/images/pembuka.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            blurRadius: 3,
+                            offset: const Offset(5, 6))
+                      ],
+                      // color: Color(0xff0B2447),
                     ),
-                    color: Color(0xff0B2447),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      const Text(
-                        "Assalamualaikum",
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
-                      Image.asset(
-                        "assets/images/dashboard.png",
-                        width: 200,
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  height: 200,
-                  // color: Colors.green,
-                  child: GridView.count(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    crossAxisCount: 2,
-                    children: [
-                      MenuDashboardCustom(
-                        nama: "Doa Harian",
-                        image: Image.asset(
-                          "assets/images/doa.png",
-                          width: 90,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text(
+                          "Assalamualaikum",
+                          style: TextStyle(fontSize: 20, color: Colors.black),
                         ),
-                        navigasi: DoaHarianScreen.routename,
-                      ),
-                      MenuDashboardCustom(
-                        nama: "Doa Harian",
-                        image: Image.asset(
-                          "assets/images/sholat.png",
-                          width: 180,
+                        Image.asset(
+                          "assets/images/dashboard.png",
+                          width: 200,
                         ),
-                        navigasi: MenuSholatScreen.routename,
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            )
-          ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 200,
+                    // color: Colors.green,
+                    child: GridView.count(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      crossAxisCount: 2,
+                      children: [
+                        MenuDashboardCustom(
+                          nama: "Doa Harian",
+                          image: Image.asset(
+                            "assets/images/doa.png",
+                            width: 90,
+                          ),
+                          navigasi: DoaHarianScreen.routename,
+                        ),
+                        MenuDashboardCustom(
+                          nama: "Doa Harian",
+                          image: Image.asset(
+                            "assets/images/sholat.png",
+                            width: 180,
+                          ),
+                          navigasi: MenuSholatScreen.routename,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
